3e97f7d1b7e6a4acd42c1a145f2a8613b0455206
Omer Gunes <omerfarukhan@gmail.com>
2015-04-05 14:01:52 +0100

    Override expressions have been removed.

--- src/cc/mallet/classify/RandomAssignmentTrainer.java
+++ src/cc/mallet/classify/RandomAssignmentTrainer.java
@@ -6,7 +6,6 @@
 
 package cc.mallet.classify;
 
-import cc.mallet.classify.ClassifierTrainer;
 import cc.mallet.pipe.Pipe;
 import cc.mallet.types.Alphabet;
 import cc.mallet.types.AlphabetCarrying;
@@ -19,74 +18,74 @@ import cc.mallet.types.Label;
  * 
  * @see RandomAssignmentTrainer
  * 
- * @author Martin Wunderlich <a href="mailto:martin@wunderlich.com">martin@wunderlich.com</a>
-*/
+ * @author Martin Wunderlich <a
+ *         href="mailto:martin@wunderlich.com">martin@wunderlich.com</a>
+ */
 public class RandomAssignmentTrainer extends ClassifierTrainer<RandomClassifier> implements AlphabetCarrying {
-	
+
 	RandomClassifier classifier = null;
-	
+
 	Pipe instancePipe;
-	Alphabet dataAlphabet;    // Extracted from InstanceList
-    Alphabet targetAlphabet; // Extracted from InstanceList
-	  
+	Alphabet dataAlphabet; // Extracted from InstanceList
+	Alphabet targetAlphabet; // Extracted from InstanceList
+
 	@Override
 	public RandomClassifier getClassifier() {
 		return this.classifier;
 	}
 
-	  /**
-	   * Create a Random classifier from a set of training data.
-	   * 
-	   * @param trainingList The InstanceList to be used to train the classifier.
-	   * @return The Random classifier as trained on the trainingList
-	   */
+	/**
+	 * Create a Random classifier from a set of training data.
+	 * 
+	 * @param trainingList
+	 *            The InstanceList to be used to train the classifier.
+	 * @return The Random classifier as trained on the trainingList
+	 */
 	@Override
 	public RandomClassifier train(InstanceList trainingList) {
-		
+
 		// Initialize or check the instancePipe
-	  	if (trainingList != null) {
-	  		if (this.instancePipe == null)
-	  			this.instancePipe = trainingList.getPipe();
-	  		else if (this.instancePipe != trainingList.getPipe())
-	  			// Make sure that this pipes match.  Is this really necessary??
-	  			// I don't think so, but it could be confusing to have each returned classifier have a different pipe?  -akm 1/08
-	  			throw new IllegalArgumentException ("Training set pipe does not match that of NaiveBayesTrainer.");
-	  		
-	  		this.dataAlphabet = this.instancePipe.getDataAlphabet();
-	  		this.targetAlphabet = this.instancePipe.getTargetAlphabet();
-	  	}
-	  	
-	  	this.classifier = new RandomClassifier(this.instancePipe);
-	  	
-	  	
-	  	// Init alphabets and extract label from instance
-		for(Instance instance : trainingList) {
+		if (trainingList != null) {
+			if (this.instancePipe == null)
+				this.instancePipe = trainingList.getPipe();
+			else if (this.instancePipe != trainingList.getPipe())
+				// Make sure that this pipes match. Is this really necessary??
+				// I don't think so, but it could be confusing to have each
+				// returned classifier have a different pipe? -akm 1/08
+				throw new IllegalArgumentException("Training set pipe does not match that of NaiveBayesTrainer.");
+
+			this.dataAlphabet = this.instancePipe.getDataAlphabet();
+			this.targetAlphabet = this.instancePipe.getTargetAlphabet();
+		}
+
+		this.classifier = new RandomClassifier(this.instancePipe);
+
+		// Init alphabets and extract label from instance
+		for (Instance instance : trainingList) {
 			if (dataAlphabet == null) {
-		  		this.dataAlphabet = instance.getDataAlphabet();
-		  		this.targetAlphabet = instance.getTargetAlphabet();
-		  	}	else if (!Alphabet.alphabetsMatch(instance, this))
-		  		// Make sure the alphabets match
-		  		throw new IllegalArgumentException ("Training set alphabets do not match those of NaiveBayesTrainer.");
-			
+				this.dataAlphabet = instance.getDataAlphabet();
+				this.targetAlphabet = instance.getTargetAlphabet();
+			} else if (!Alphabet.alphabetsMatch(instance, this))
+				// Make sure the alphabets match
+				throw new IllegalArgumentException("Training set alphabets do not match those of NaiveBayesTrainer.");
+
 			Label label = (Label) instance.getTarget();
-			
+
 			this.classifier.addTargetLabel(label);
 		}
-		
+
 		return this.classifier;
 	}
 
 	// AlphabetCarrying interface
 	public boolean alphabetsMatch(AlphabetCarrying object) {
-		return Alphabet.alphabetsMatch (this, object);
+		return Alphabet.alphabetsMatch(this, object);
 	}
-	
-	@Override
+
 	public Alphabet getAlphabet() {
 		return dataAlphabet;
 	}
-	
-	@Override
+
 	public Alphabet[] getAlphabets() {
 		return new Alphabet[] { dataAlphabet, targetAlphabet };
 	}
